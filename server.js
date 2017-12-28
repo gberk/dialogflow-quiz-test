f68aadd43701e130058b102ece3ecb7c6f4304a1
kevin.lee@refreshlabs.co <kevin.lee@lynkr.xyz>
2017-12-28 14:55:52 -0800

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-var famousPeopleQuiz = require('./famousPeopleQuiz');
+var stateQuiz = require('./stateQuiz');
 var rp = require('request-promise');
 require('dotenv').config();
 
@@ -12,21 +12,35 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 // app.use(express.static('client'));
 app.listen(process.env.PORT||4000);
 
+const NAME_GAME = 'nameGame';
+const TURNS_ARG = 'turns';
+
 app.post('/guess', function(req,res){
     
     var intent = req.body.result.action;
     console.log(intent)
     var result = dialogflowResponse();
+
     if (intent === 'input.welcome') {
-        result.speech = "Welcome to Trivia."
+        result.speech = "Welcome to Trivia. What game would you like to play?"
     }
 
-    // var guess = req.body.result.parameters.guess;
-    // var answers = famousPeopleQuiz.questions[0].answers;
-    // var result = dialogflowResponse();
-    // var answer = isAnAnswer(guess,answers);
-    // result.speech = answer ? answer.key : "Not an answer";
-    // console.log(result)
+    else if (intent === 'startNameGame') {
+        result.contextOut = [{"name":NAME_GAME, "lifespan":2, "parameters":{TURNS_ARG:5}}];
+        result.speech = "Name 5 states in the United States";
+    }
+
+    else if (intent === 'guess') {
+        var guess = req.body.result.parameters.guess;
+        var answers = famousPeopleQuiz.questions[0].answers;
+        var result = dialogflowResponse();
+        var answer = isAnAnswer(guess,answers);
+        result.speech = answer ? answer.key : "Not an answer";
+    }
+    else {
+        result.speech = "We had a problem doing a flip."
+    }
+
     res.send(result)
 })
 
